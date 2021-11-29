69a600e9c915c56dbe2916c26f18d5770ab4fc38
Ciibelle <85749588+Ciibelle@users.noreply.github.com>
2021-11-29 16:22:08 -0300

    Add files via upload

---
+++ Atv-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Atv_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O contais é o metódo de verdadeiro ou falso.
+            //(true) se o parâmetro value ocorrer dentro dessa instância; caso contrário, (false).
+            
+            string curso = "Meu computador é velho";
+            Console.WriteLine($"Frase antes do uso do contains: {curso}");
+
+            Console.WriteLine($"A frase contém a palavra computador?: {curso.Contains("computador")}");
+
+        }
+    }
+}
